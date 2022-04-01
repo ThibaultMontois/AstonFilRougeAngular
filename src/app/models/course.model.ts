d04f5c203809433ce1e45e712fa5427cc090b1d9
ThibaultMontois <thibault.montois@gmail.com>
2022-04-01 15:59:46 +0200

    feat: database rearrangement

--- src/app/models/course.model.ts
+++ src/app/models/course.model.ts
@@ -2,7 +2,6 @@ import { Time } from "@angular/common";
 import { CourseStatus } from "../enums/course-status.enum";
 import { CourseType } from "../enums/course-type.enum";
 import { Role } from "../enums/role.enum";
-import { Address } from "./address.model";
 import { User } from "./user.model";
 
 export class Course {
@@ -13,21 +12,19 @@ export class Course {
     endHour: Time;
     status: CourseStatus;
     clubId: number;
-    address: Address | null;
     coachId: number;
     limit: number;
     title: string;
     description: string;
 
-    constructor(id: number, type: CourseType, date: Date, startHour: Time, endHour: Time, status: CourseStatus, clubId?: number, address?: Address, coach?: User, limit?: number, title?: string, description?: string) {
+    constructor(id: number, type: CourseType, date: Date, startHour: Time, endHour: Time, status: CourseStatus, clubId?: number, coach?: User, limit?: number, title?: string, description?: string) {
         this.id = id;
         this.type = type;
         this.date = date;
         this.startHour = startHour;
         this.endHour = endHour;
         this.status = status;
-        this.clubId = this.type !== CourseType.Home ? clubId ?? 0 : 0;
-        this.address = this.type === CourseType.Home ? address ?? null : null;
+        this.clubId = clubId ?? 0;
         this.coachId = this.type !== CourseType.Access && coach && coach.role === Role.Coach ? coach.id : 0;
         this.limit = this.type === CourseType.Collective ? limit ?? 1 : 1;
         this.title = this.type === CourseType.Collective ? title ?? '' : '';
--- src/app/models/reservation.model.ts
+++ src/app/models/reservation.model.ts
@@ -1,17 +1,16 @@
 import { ReservationStatus } from "../enums/reservation-status.enum";
-import { Course } from "./course.model";
 
 export class Reservation {
     id: number;
     clientId: number;
-    course: Course;
+    courseId: number;
     status: ReservationStatus;
     requestDate: Date;
 
-    constructor(id: number, clientId: number, course: Course, status?: ReservationStatus) {
+    constructor(id: number, clientId: number, courseId: number, status?: ReservationStatus) {
         this.id = id;
         this.clientId = clientId;
-        this.course = course;
+        this.courseId = courseId;
         this.status = status ?? ReservationStatus.Requested;
         this.requestDate = new Date();
     }
