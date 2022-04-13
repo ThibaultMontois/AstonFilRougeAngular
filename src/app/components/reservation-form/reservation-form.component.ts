1c9b6ea490fd6d6bbfab0d69ccda038aaf99c78d
Loic Coornaert <71696114+Loiccoornaert27@users.noreply.github.com>
2022-04-13 11:25:50 +0200

    Update reservation-form.component.ts

--- src/app/components/reservation-form/reservation-form.component.ts
+++ src/app/components/reservation-form/reservation-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthUser } from 'src/app/models/auth-user.model';
 import { Course } from 'src/app/models/course.model';
 import { Reservation } from 'src/app/models/reservation.model';
@@ -17,7 +18,7 @@ export class ReservationFormComponent implements OnInit {
   formGroup!: FormGroup;
   private courseList?: Course[];
   private authUser?: AuthUser | null = this.authService.user;
-  constructor(private fb: FormBuilder, private dbService: DatabaseService, private authService: AuthUserService) { }
+  constructor(private fb: FormBuilder, private dbService: DatabaseService, private authService: AuthUserService, private router: Router) { }
 
   ngOnInit(): void {
     this.getCourseList();
@@ -33,13 +34,15 @@ export class ReservationFormComponent implements OnInit {
 
     let user: User;
 
+    if(this.authUser?.email!=null){
+      this.dbService.getUserByEmail(this.authUser.email).subscribe(client => {
 
-    this.dbService.getUserByEmail(authUser.email).subscribe(client => {
-
-      user = client
-      let resa: Reservation = new Reservation(0, user.id, selectedCourse);
-      this.dbService.createReservation(resa);
-    });
+        user = client
+        let resa: Reservation = new Reservation(0, user.id, selectedCourse);
+        this.dbService.createReservation(resa);
+      });
+    }
+   
   }
 
   onCancel(): void {
